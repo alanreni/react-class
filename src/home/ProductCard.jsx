6559233fd406f193f778b791a406603d9d909edd
Ashwin Shenoy <aswinshenoy65@gmail.com>
2025-06-25 12:32:06 +0530

    update product card

--- src/home/ProductCard.jsx
+++ src/home/ProductCard.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 
-const ProductCard = ({ id, name }) => {
+const ProductCard = ({ id, name, imageURL, price }) => {
 
     return (
         <Link to={`/product/${id}`}>
-            <div className="p-2 border shadow">
+            {imageURL ? (
+                <img src={imageURL} alt="" />
+            ) : (
+                <div>Placeholder</div>
+            )}
+            <div className="md:text-red-500 border-purple-500 text-lg text-left md:text-center font-semibold p-2 rounded-lg border shadow">
                 {name}
             </div>
+            <div>Rs. {price}</div>
         </Link>
     );
 
